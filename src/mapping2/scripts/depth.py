60fcdb8a10536e6c1924674d25e795ce7fe24893
Om Kumar <128745874+W-OK-E@users.noreply.github.com>
2025-01-26 20:20:38 +0530

    Update depth.py

--- src/mapping2/scripts/depth.py
+++ src/mapping2/scripts/depth.py
@@ -1,12 +1,5 @@
-import cv2
-import torch
-import os
-import numpy as np
-import matplotlib.pyplot as plt
-
-import numpy as np
-from scipy.ndimage import gaussian_filter
-
+import torch,cv2
+import argparse,os
 from depth_anything_v2.dpt import DepthAnythingV2
 
 def get_depth(img):
@@ -25,3 +18,26 @@ def get_depth(img):
     model.load_state_dict(torch.load(f'/media/summer/ubuntu 2tb/ISDC_MAIN/MONO_2/src/depth/checkpoints/depth_anything_v2_{encoder}.pth', map_location='cpu'))
     model = model.to(DEVICE).eval()
     return model.infer_image(img)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script to convert Monocular to depth image"
+    )
+    parser.add_argument(
+        "--im_path",
+        type = str,
+        required = True,
+        help = 'Path of the Image whose depth needs to be calculated'
+    )
+
+    args = parser.parse_args()
+    fname = args.im_path
+    depth_image = get_depth(cv2.imread(fname))
+    
+    if(os.path.exists("results")):
+        os.mkdir("results")
+    
+    cv2.imwrite("results/depth.png",depth_image)
+if __name__ == "__main__":
+    main()
